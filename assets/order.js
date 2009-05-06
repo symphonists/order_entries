22034077bf544ddbebe24a3e0da945a421f1f91d
Nick Dunn <nickdunn@nick-dunn-2.local>
2009-05-06 21:00:06 +0100

    JavaScript updated with ahwayakchih's suggestions. More succinct and better use of jQuery selectors.

--- assets/order.js
+++ assets/order.js
@@ -1,111 +1,115 @@
-(function($) {
+jQuery(document).ready(function() {
+    var $ = jQuery;
 
-	var movable = {
-		move: function(e) {
-			var t,
-			    n,
-			    y = e.pageY;
+    var table = $("table:first")
+    var rows = $("tbody tr", table)
 
-			if (y < movable.min) {
-				t = movable.target.prev();
-				for (;;) {
-					movable.delta--;
-					n = t.prev();
-					if (n.length === 0 || y >= (movable.min -= n.height())) {
-						movable.target.insertBefore(t);
-						break;
-					}
-					t = n;
-				}
-			} else if (y > movable.max) {
-				t = movable.target.next();
-				for (;;) {
-					movable.delta++;
-					n = t.next();
-					if (n.length === 0 || y <= (movable.max += n.height())) {
-						movable.target.insertAfter(t);
-						break;
-					}
-					t = n;
-				}
-			} else {
-				return;
-			}
+    if (!table || rows.length == 0 || !$("#order_number_field").text()) return;
+    
+    if ($("form:first").attr("action").indexOf("/publish/") == -1) {
+        return false;
+    }
+    
+    $("h2:first *:first").before(" (drag to reorder) ");
 
-			movable.update(movable.target);
-			movable.target.parent().children().each(function(i) { $(this).toggleClass('odd', i % 2 === 0); });
-		},
-		drop: function() {
-			$(document).unbind('mousemove', movable.move);
-			$(document).unbind('mouseup', movable.drop);
+    $(table).addClass("order-entries");
 
-			movable.target.removeClass('movable');
+    // Sortable lists - copied from Symphony's admin.js, because it's not accessible for us (too bad it wasn't made as Symphony.movable :(.
+    var movable = {
+        move: function(e) {
+            var t,
+                n,
+                y = e.pageY;
 
-			if (movable.delta) {
-				movable.target.trigger($.Event('reorder'));
-			}
-		},
-		update: function(target) {
-			var a = target.height(),
-			    b = target.offset().top;
+            if (y < movable.min) {
+                t = movable.target.prev();
+                for (;;) {
+                    movable.delta--;
+                    n = t.prev();
+                    if (n.length === 0 || y >= (movable.min -= n.height())) {
+                        movable.target.insertBefore(t);
+                        break;
+                    }
+                    t = n;
+                }
+            } else if (y > movable.max) {
+                t = movable.target.next();
+                for (;;) {
+                    movable.delta++;
+                    n = t.next();
+                    if (n.length === 0 || y <= (movable.max += n.height())) {
+                        movable.target.insertAfter(t);
+                        break;
+                    }
+                    t = n;
+                }
+            } else {
+                return;
+            }
 
-			movable.target = target;
-			movable.min    = Math.min(b, a + (target.prev().offset().top || -Infinity));
-			movable.max    = Math.max(a + b, b + (target.next().height() ||  Infinity));
-		}
-	};
-	
+            movable.update(movable.target);
+            movable.target.parent().children().each(function(i) { $(this).toggleClass('odd', i % 2 === 0); });
+        },
+        drop: function() {
+            $(document).unbind('mousemove', movable.move);
+            $(document).unbind('mouseup', movable.drop);
 
-	$(document).ready(function() {
-		
-		var h2 = $("h2:first");
-		var h2_text = h2.html();
-		h2.html(h2_text + " (drag to reorder)");
+            movable.target.removeClass('movable');
 
-		$('table tr').live('mousedown', function(e) {
-			if (!/^(?:h4|td)$/i.test(e.target.nodeName)) {
-				return true;
-			}
+            if (movable.delta) {
+                movable.target.trigger($.Event('reorder'));
+            }
+        },
+        update: function(target) {
+            var a = target.height(),
+                b = target.offset().top;
 
-			movable.update($(this).addClass('movable'));
-			movable.delta = 0;
+            movable.target = target;
+            movable.min    = Math.min(b, a + (target.prev().offset().top || -Infinity));
+            movable.max    = Math.max(a + b, b + (target.next().height() ||  Infinity));
+        }
+    };
 
-			$(document).mousemove(movable.move);
-			$(document).mouseup(movable.drop);
 
-			return false;
-		});
+    // Based on code from Symphony's admin.js
+    $('.order-entries tr').live('mousedown', function(e) {
+        if (!/^(?:h4|td)$/i.test(e.target.nodeName)) {
+            return true;
+        }
 
-		$('table.selectable').live('reorder', function() {
-			var t = $(this).addClass('busy');
+        movable.update($(this).addClass('movable'));
+        movable.delta = 0;
 
-			var column_index = 0;
-			t.find('thead th').each(function(i) {
-				var a = $("a.active", this);
-				if (a.length) {
-					column_index = i;
-				}
-			});			
+        $(document).mousemove(movable.move);
+        $(document).mouseup(movable.drop);
 
-			t.find("tbody tr").each(function(i) {
-				$(this).find('td:eq(' + column_index + ')').text(i + 1);
-			});
-			
-			$.ajax({
-				type: 'POST',
-				url: Symphony.WEBSITE + '/symphony/extension/order_entries/save/',
-				data: $('input', this).map(function(i) { return this.name + '=' + (i + 1); }).get().join('&') + '&field=' + $('#order_number_field').text(),
-				complete: function(x) {
-					if (x.status === 200) {
-						Symphony.Message.clear('reorder');
-					} else {
-						Symphony.Message.post(Symphony.Language.REORDER_ERROR, 'reorder error');
-					}
-					t.removeClass('busy');
-				}
-			});
-		});
+        return false;
+    });
 
-	});
-		
-})(jQuery.noConflict());
+    $('table.order-entries').live('reorder', function() {
+        var t = $(this).addClass('busy');
+        $.ajax({
+            type: 'POST',
+            url: Symphony.WEBSITE + "/symphony/extension/order_entries/save/",
+            data: $('input', this).map(function(e, i) { return this.name + '=' + (e + 1); }).get().join('&') + "&field=" + $("#order_number_field").text(),
+            complete: function(x) {
+                if (x.status === 200) {
+                    Symphony.Message.clear('reorder');
+
+                    // find the Order Field column index
+                    var column_index = $("thead th a.active[href*='&sort="+$("#order_number_field").text()+"&']", table).parent().prevAll().length;
+                    $("tbody tr td:nth-child("+(column_index+1)+")", table).each(function(i) {
+                        $(this).text((i+1));
+                    });
+
+                    // deselect rows
+                    $("tr.selected td:first", table).trigger($.Event('click'));
+
+                } else {
+                    Symphony.Message.post(Symphony.Language.REORDER_ERROR, 'reorder error');
+                }
+                t.removeClass('busy');
+            }
+        });
+    });
+});
