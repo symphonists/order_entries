90105957d573f279db4edc98ce14f82a82dc110f
Nick Dunn <nick@nick-dunn.co.uk>
2009-08-27 16:42:25 +0100

    Fix ON DUPLICATE KEY UPDATE bug.

--- content/content.save.php
+++ content/content.save.php
@@ -17,7 +17,13 @@
 		    if(!is_array($items) || empty($items)) exit;
 
 			foreach($items as $entry_id => $position) {
-		        $this->_Parent->Database->query("INSERT INTO tbl_entries_data_$field_id (entry_id, value) VALUES ('$entry_id', '$position') ON DUPLICATE KEY UPDATE `entry_id` = '$entry_id'");
+				$id = $this->_Parent->Database->fetchVar('id', 0, "SELECT id FROM tbl_entries_data_$field_id WHERE `entry_id` = '$entry_id' ORDER BY id ASC LIMIT 1");
+				if (is_null($id)) {
+					$this->_Parent->Database->query("INSERT INTO tbl_entries_data_$field_id (entry_id, value) VALUES ('$entry_id', '$position')");
+				} else {
+					$this->_Parent->Database->query("UPDATE tbl_entries_data_$field_id SET `value`='$position' WHERE `entry_id` = '$entry_id'");
+					$this->_Parent->Database->query("DELETE FROM tbl_entries_data_$field_id WHERE `entry_id` = '$entry_id' AND `id` > '$id'");
+				}
 		    }
 			
 			exit;
