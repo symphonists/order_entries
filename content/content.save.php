9dbbacb7b74d7ea067486f90adbda436bc310364
Nick Dunn <nick.dunn@airlock.com>
2011-02-06 16:29:39 +0000

    Updated SQL syntax

--- content/content.save.php
+++ content/content.save.php
@@ -17,12 +17,12 @@
 		    if(!is_array($items) || empty($items)) exit;
 
 			foreach($items as $entry_id => $position) {
-				$id = $this->_Parent->Database->fetchVar('id', 0, "SELECT id FROM tbl_entries_data_$field_id WHERE `entry_id` = '$entry_id' ORDER BY id ASC LIMIT 1");
+				$id = Symphony::Database()->fetchVar('id', 0, "SELECT id FROM tbl_entries_data_$field_id WHERE `entry_id` = '$entry_id' ORDER BY id ASC LIMIT 1");
 				if (is_null($id)) {
-					$this->_Parent->Database->query("INSERT INTO tbl_entries_data_$field_id (entry_id, value) VALUES ('$entry_id', '$position')");
+					Symphony::Database()->query("INSERT INTO tbl_entries_data_$field_id (entry_id, value) VALUES ('$entry_id', '$position')");
 				} else {
-					$this->_Parent->Database->query("UPDATE tbl_entries_data_$field_id SET `value`='$position' WHERE `entry_id` = '$entry_id'");
-					$this->_Parent->Database->query("DELETE FROM tbl_entries_data_$field_id WHERE `entry_id` = '$entry_id' AND `id` > '$id'");
+					Symphony::Database()->query("UPDATE tbl_entries_data_$field_id SET `value`='$position' WHERE `entry_id` = '$entry_id'");
+					Symphony::Database()->query("DELETE FROM tbl_entries_data_$field_id WHERE `entry_id` = '$entry_id' AND `id` > '$id'");
 				}
 		    }
 			
