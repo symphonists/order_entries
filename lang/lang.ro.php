1a43b51aa0bbdb8df58ddd105a0755f3da1c8f0b
Vlad <vlad_micutul@yahoo.com>
2011-02-09 23:14:09 +0800

    Added Romanian translation.

---
+++ lang/lang.ro.php
@@ -0,0 +1,33 @@
+<?php
+
+	$about = array(
+		'name' => '',
+		'author' => array(
+			'name' => 'Vlad Ghita',
+			'email' => 'vlad_micutul@yahoo.com',
+			'website' => ''
+		),
+		'release-date' => '2011-02-09'
+	);
+
+	/**
+	 * Order Entries
+	 */
+	$dictionary = array(
+	
+		'drag to reorder' => 
+		'trage pentru rearanjare',
+
+		'Entry order saved.' => 
+		'Ordinea înregistrărilor salvată.',
+
+		'Entry Order' => 
+		'Sortare Înregistrări',
+
+		'%s Disable sorting of other columns when enabled' =>
+		'%s Când este activată, dezactivează sortarea prin celelalte coloane ',
+
+		'%s Hide this field on publish page' =>
+		'%s Ascunde acest câmp pe pagina de publicare',
+
+	);
