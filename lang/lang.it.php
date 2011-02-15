ddabf97805cf53f61c32331590578085fea6cdfb
Nick Dunn <nick.dunn@airlock.com>
2011-02-15 17:15:06 +0000

    Added Italian translation

---
+++ lang/lang.it.php
@@ -0,0 +1,40 @@
+<?php
+
+	$about = array(
+		'name' => 'Italiano',
+		'author' => array(
+			'name' => 'Simone Economo',
+			'email' => 'my.ekoes@gmail.com',
+			'website' => 'http://lineheight.net',
+		),
+	);
+	
+	
+	/*
+	 * EXTENSION: Order entries
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+
+		'Entry Order' =>
+		'Ordinamento',
+
+		'%s Disable sorting of other columns when enabled' =>
+		'%s Disabilita il riordinamento per colonne',
+
+		'%s Hide this field on publish page' =>
+		'%s Rendi il campo invisibile nell\'editor delle voci',
+
+		'To filter by ranges, add <code>%s</code> to the beginning of the filter input. Use <code>%s</code> for field name. E.G. <code>%s</code>' =>
+		'Per selezionare un intervallo, aggiungi <code>%s</code> all\'inizio della riga e usa <code>%s</code> per indicare la posizione. Esempio: <code>%s</code>',
+
+		'Must be a number' =>
+		'Deve essere un numero',
+
+		'drag to reorder' =>
+		'trascina per riordinare'
+
+	);
+
+?>
