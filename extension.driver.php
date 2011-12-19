7b04f7769933a66232899c4d6f71d7bef80b485b
Nick Dunn <nick@nick-dunn.co.uk>
2011-12-19 16:53:36 +0000

    Updated meta for 1.9.8

--- extension.driver.php
+++ extension.driver.php
@@ -4,8 +4,8 @@
 	
 		public function about(){
 			return array('name' => 'Order Entries',
-						 'version' => '1.9.7',
-						 'release-date' => '2011-10-12',
+						 'version' => '1.9.8',
+						 'release-date' => '2011-12-19',
 						 'author' => array('name' => 'Nick Dunn',
 										   'website' => 'http://nick-dunn.co.uk')
 				 		);
