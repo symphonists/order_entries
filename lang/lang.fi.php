60970a806c80f5f6cf024f3912d385aba28cfe05
lnikkila <leo.nikkila@gmail.com>
2013-06-22 17:51:58 +0300

    Added Finnish translation.

---
+++ lang/lang.fi.php
@@ -0,0 +1,34 @@
+<?php
+
+	$about = array(
+		'name' => 'Finnish',
+		'author' => array(
+			'name' => 'Leo Nikkilä',
+			'email' => 'leo.nikkila@gmail.com',
+			'website' => ''
+		),
+		'release-date' => '2013-06-22'
+	);
+
+	/**
+	 * Order Entries
+	 */
+	$dictionary = array(
+
+		'Entry order saved.' => 
+		'Järjestys tallennettu.',
+
+		'Entry Order' => 
+		'Merkintöjen järjestäminen',
+		
+		'%s Disable sorting of other columns when enabled' => 
+		'%s Estä muiden sarakkeiden perusteella järjestäminen 
+kun kytketty päälle',
+
+		'%s Hide this field on publish page' => 
+		'%s Piilota tämä kenttä muokkaussivulla',
+		
+		'drag to reorder' =>
+		'vedä ja pudota järjestääksesi uudelleen'
+		
+	);
