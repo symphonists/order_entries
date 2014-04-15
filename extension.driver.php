632f96b3eb6ae9510e869f49008c17f124547065
Nils Hörrmann <post@nilshoerrmann.de>
2014-04-15 18:59:22 +0200

    Update extension for Symphony 2.4, ensure consistent formatting
    Fixes #11, closes #41

--- extension.driver.php
+++ extension.driver.php
@@ -1,81 +1,154 @@
 <?php
 
-	Class extension_order_entries extends Extension{
+	Class extension_order_entries extends Extension {
+
+		private $force_sort = false;
+		private $pagination_maximum_rows = null;
 		
+		/**
+		 * {@inheritDoc}
+		 */
 		public function getSubscribedDelegates(){
 			return array(
-					array(
-						'page' => '/backend/',
-						'delegate' => 'InitaliseAdminPageHead',
-						'callback' => 'appendScriptToHead'
-					)
+				array(
+					'page' => '/backend/',
+					'delegate' => 'InitaliseAdminPageHead',
+					'callback' => 'prepareIndex'
+				),
+				array(
+					'page' => '/publish/',
+					'delegate' => 'AddCustomPublishColumn',
+					'callback' => 'adjustTable'
+				),
+				array(
+					'page' => '/backend/',
+					'delegate' => 'AdminPagePostGenerate',
+					'callback' => 'resetPagination'
+				)
 			);
 		}
 		
-		public function appendScriptToHead($context){
-			
-			$page_callback = Administration::instance()->getPageCallback();
-			$page_callback = $page_callback['context'];
-			
-			if(isset($page_callback['section_handle']) && $page_callback['page'] == 'index'){
+		/**
+		 * Prepare publish index for manual entry ordering
+		 */
+		public function prepareIndex($context) {
+			$callback = Symphony::Engine()->getPageCallback();
+
+			if($callback['driver'] == 'publish' && $callback['context']['page'] == 'index') {
 				
-				// find sort settings for this section (sort field ID and direction)
-				$section_id = SectionManager::fetchIDFromHandle($page_callback['section_handle']);
-				if(!$section_id) return;
-				
-				$section = SectionManager::fetch(SectionManager::fetchIDFromHandle($page_callback['section_handle']));
-				
-				// we only want a valid entry order field and ascending order only
-				if ($section->getSortingOrder() !== 'asc' || !is_numeric($section->getSortingField())) return;
-				
-				$field = FieldManager::fetch($section->getSortingField());
-				if(!$field || $field->get('type') !== 'order_entries') return;
-				
-				Administration::instance()->Page->addElementToHead(
-					new XMLElement(
-						'script',
-						"Symphony.Context.add('order-entries', " . json_encode(array(
-							'id' => $field->get('id'),
-							'force-sort' => $field->get('force_sort'),
-						)) . ");",
-						array('type' => 'text/javascript')
-					), time()
-				);
-				
-				Administration::instance()->Page->addScriptToHead(
-					URL . '/extensions/order_entries/assets/order_entries.publish.js',
-					time()
-				);
-				
-				Symphony::Configuration()->set("pagination_maximum_rows", 99999, "symphony");
-				
+				// Fetch sort settings for this section (sort field ID and direction)
+				$section_id = SectionManager::fetchIDFromHandle($callback['context']['section_handle']);
+
+				// Fetch sorting field
+				if($section_id) {
+					$section = SectionManager::fetch($section_id);
+					$field = FieldManager::fetch($section->getSortingField());
+
+					// Check sorting field
+					if($field->get('type') == 'order_entries') {
+						$this->force_sort = $field->get('force_sort');
+					
+						// Initialise manual ordering
+						$this->addComponents();
+						$this->disablePagination();
+					}
+				}
 			}
-			
 		}
 		
-		public function uninstall(){
-			Symphony::Database()->query("DROP TABLE `tbl_fields_order_entries`");
+		/**
+		 * Force manual sorting
+		 */
+		public function adjustTable($context) {
+			if($this->force_sort == 'yes') {
+				foreach($context['tableHead'] as $head) {
+					if($head[2]['class'] == 'field-order_entries') {
+						$head[0]->setAttribute('data-manual-sort', 'force');
+					}
+				}
+			}
+		}
+		
+		/**
+		 * Add components for manual entry ordering
+		 */
+		public function addComponents() {
+			Administration::instance()->Page->addScriptToHead(
+				APPLICATION_URL . '/extensions/order_entries/assets/order_entries.publish.js'
+			);
 		}
 
-		public function update($previousVersion){
-			if(version_compare($previousVersion, '1.6', '<')){
-				Symphony::Database()->query("ALTER TABLE `tbl_fields_order_entries` ADD `force_sort` enum('yes','no') DEFAULT 'no'");
+		/**
+		 * Contextually adjust maximum pagination rows
+		 */
+		public function disablePagination() {
+			$this->pagination_maximum_rows = Symphony::Configuration()->get('pagination_maximum_rows', 'symphony');
+			Symphony::Configuration()->set('pagination_maximum_rows', 99999, 'symphony');
+		}
+
+		/**
+		 * Reset maximum pagination rows
+		 */
+		public function resetPagination() {
+			if($this->pagination_maximum_rows !== null) {
+				Symphony::Configuration()->set('pagination_maximum_rows', $this->pagination_maximum_rows, 'symphony');
 			}
-			if(version_compare($previousVersion, '1.8', '<')){
-				Symphony::Database()->query("ALTER TABLE `tbl_fields_order_entries` ADD `hide` enum('yes','no') DEFAULT 'no'");
-			}
-			return true;
+		}
+			
+		/**
+		 * {@inheritDoc}
+		 */
+		public function uninstall() {
+			Symphony::Database()->query("DROP TABLE `tbl_fields_order_entries`");
 		}
 		
-		public function install(){
-			return Symphony::Database()->query("CREATE TABLE `tbl_fields_order_entries` (
-			  `id` int(11) unsigned NOT NULL auto_increment,
-			  `field_id` int(11) unsigned NOT NULL,
-			  `force_sort` enum('yes','no') default 'no',
-			  `hide` enum('yes','no') default 'no',
-			  PRIMARY KEY  (`id`),
-			  UNIQUE KEY `field_id` (`field_id`)
-			) TYPE=MyISAM");
+		/**
+		 * {@inheritDoc}
+		 */
+		public function update($previousVersion) {
+			$status = array();
+
+			// Prior version 1.6
+			if(version_compare($previousVersion, '1.6', '<')) {
+				$status[] = Symphony::Database()->query("
+					ALTER TABLE `tbl_fields_order_entries`
+					ADD `force_sort` enum('yes','no')
+					DEFAULT 'no'
+				");
+			}
+			
+			// Prior version 1.8
+			if(version_compare($previousVersion, '1.8', '<')) {
+				$status[] = Symphony::Database()->query("
+					ALTER TABLE `tbl_fields_order_entries`
+					ADD `hide` enum('yes','no')
+					DEFAULT 'no'
+				");
+			}
+
+			// Report status
+			if(in_array(false, $status, true)) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+		
+		/**
+		 * {@inheritDoc}
+		 */
+		public function install() {
+			return Symphony::Database()->query("
+				CREATE TABLE `tbl_fields_order_entries` (
+					`id` int(11) unsigned NOT NULL auto_increment,
+					`field_id` int(11) unsigned NOT NULL,
+					`force_sort` enum('yes','no') default 'no',
+					`hide` enum('yes','no') default 'no',
+					PRIMARY KEY  (`id`),
+					UNIQUE KEY `field_id` (`field_id`)
+				) TYPE=MyISAM
+			");
 		}
 			
 	}
