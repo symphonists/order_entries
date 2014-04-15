07347e52d289c5ec7fd37ac6523d26f84a9d2428
Nils Hörrmann <post@nilshoerrmann.de>
2014-04-15 20:33:23 +0200

    Make use of JSONPage

--- content/content.save.php
+++ content/content.save.php
@@ -1,29 +1,47 @@
 <?php
 
-	require_once(TOOLKIT . '/class.administrationpage.php');
-	
-	class ContentExtensionOrder_EntriesSave extends AdministrationPage {
-		
-		public function __viewIndex() {
-			$field_id = $_REQUEST["field"];
-			$items = $_REQUEST['items'];
-			
-		    if(!is_array($items) || empty($items)) exit;
+	require_once(TOOLKIT . '/class.jsonpage.php');
 
-			Symphony::ExtensionManager()->notifyMembers('EntryPreEdit', '/publish/edit/');
+	Class contentExtensionOrder_entriesSave extends JSONPage{
+		
+		public function view(){
+			$field_id = General::sanitize($_GET['field']);
+			$items = $_GET['items'];
+
+			if(!is_array($items) || empty($items)) {
+				$this->_Result['error'] = __('No items provided');
+				$this->generate();
+			};
 
 			foreach($items as $entry_id => $position) {
-				$id = Symphony::Database()->fetchVar('id', 0, "SELECT id FROM tbl_entries_data_$field_id WHERE `entry_id` = '$entry_id' ORDER BY id ASC LIMIT 1");
-				if (is_null($id)) {
-					Symphony::Database()->query("INSERT INTO tbl_entries_data_$field_id (entry_id, value) VALUES ('$entry_id', '$position')");
-				} else {
-					Symphony::Database()->query("UPDATE tbl_entries_data_$field_id SET `value`='$position' WHERE `entry_id` = '$entry_id'");
-					Symphony::Database()->query("DELETE FROM tbl_entries_data_$field_id WHERE `entry_id` = '$entry_id' AND `id` > '$id'");
+				$id = Symphony::Database()->fetchVar('id', 0, "
+					SELECT id
+					FROM tbl_entries_data_$field_id
+					WHERE `entry_id` = '$entry_id'
+					ORDER BY id ASC LIMIT 1
+				");
+
+				if(is_null($id)) {
+					Symphony::Database()->query("
+						INSERT INTO tbl_entries_data_$field_id (entry_id, value)
+						VALUES ('$entry_id', '$position')
+					");
+				}
+				else {
+					Symphony::Database()->query("
+						UPDATE tbl_entries_data_$field_id
+						SET `value`='$position'
+						WHERE `entry_id` = '$entry_id'
+					");
+					Symphony::Database()->query("
+						DELETE FROM tbl_entries_data_$field_id
+						WHERE `entry_id` = '$entry_id'
+						AND `id` > '$id'
+					");
 				}
 		    }
-			
-			Symphony::ExtensionManager()->notifyMembers('EntryPostEdit', '/publish/edit/');
-
-			exit;
+	
+			$this->_Result['success'] = __('Sorting complete');
 		}
+		
 	}
